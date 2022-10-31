9f842cdb9c3cb7215a7627e3df727aaced9954ae
Muhammet Eser <114760160+muhammetser@users.noreply.github.com>
2022-10-31 17:33:27 +0300

    Add files via upload

---
+++ JAVA/Sayinin4Ve5KuvvetleriniBulma/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Sınır sayısını giriniz : ");
+        n = inp.nextInt();
+
+        System.out.println("4'ün Kuvvetleri;");
+
+        for (int i = 1; i <= n; i *= 4) {
+            System.out.println(i);
+        }
+
+        System.out.println("5'in Kuvvetleri;");
+        for (int i = 1; i <= n; i *=5) {
+            System.out.println(i);
+        }
+    }
+}

2ab18b17781202a40415b7031785b27ed5872eb7
Muhammet Eser <114760160+muhammetser@users.noreply.github.com>
2022-11-03 06:47:56 +0300

    Add files via upload

---
+++ JAVA/Fibonacci/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Lütfen eleman sayısını giriniz : ");
+
+        int sayi = scan.nextInt();
+        int n1 = 0,n2 = 1;
+        System.out.print(n1 +" " +n2 + " ");
+        for(int i = 1;i <= (sayi - 2);i++){
+            int result = n1 + n2;
+            System.out.print(result + " ");
+            n1 = n2;
+            n2 = result;
+        }
+
+    }
+}

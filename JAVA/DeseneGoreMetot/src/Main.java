047c56733a48c34a2ee3c2b226561eaf66b32577
Muhammet Eser <114760160+muhammetser@users.noreply.github.com>
2022-11-06 16:46:45 +0300

    Add files via upload

---
+++ JAVA/DeseneGoreMetot/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Main {
+    static void cikti(int n){
+        if (n > 0){
+            System.out.print(n + " ");
+
+            cikti(n - 5);
+        }System.out.print(n +" ");
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("N sayısı : ");
+        int n = scan.nextInt();
+        System.out.print("Çıktısı : ");
+        cikti(n);
+        System.out.println();
+        scan.close();
+    }
+}
